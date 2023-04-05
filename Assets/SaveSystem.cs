54231a35e989ecb1678bfc3b04dff24531dfc7e3
L M <80474693+Loris-Moreau@users.noreply.github.com>
2023-04-05 18:17:00 +0200

    Binary saver

--- Assets/SaveSystem.cs
+++ Assets/SaveSystem.cs
@@ -6,20 +6,38 @@ public class SaveSystem : MonoBehaviour
 {
 	//information variables : (HP, Player Lvl, Game Lvl, Player Name/Character, Inventory, etc...)
 	//need to save variables in the same order that they are loaded  &  not doward/upward compatible (i.e. when you add/remove variables from your saveGame it becomes corrupt)
-	public int HP = 7;
-
+	public int HP = 10;
+	public string _name = "Bob";
+	
 	void Update()
 	{
-		if(Input.GetKeyDown(KeyCode.S))
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			string saveFilePath = Application.persistentDataPath + "/saveGame.sav";
+			print("Saving to : " + saveFilePath);
+
+			FileStream fs = new FileStream(saveFilePath, FileMode.OpenOrCreate);
+			BinaryWriter sw = new BinaryWriter(fs);
+
+			sw.Write(HP);
+			sw.Write(_name);
+
+			fs.Close();
+			sw.Close();
+		}
+
+		if(Input.GetKeyDown(KeyCode.L))
 		{
 			string saveFilePath = Application.persistentDataPath + "/saveGame.sav";
 
-			print("saving to : " + saveFilePath);
+			FileStream fs = new FileStream (saveFilePath, FileMode.Open);
+			BinaryReader sr = new BinaryReader(fs);
 
-			StreamWriter sw = new StreamWriter(saveFilePath);
-			sw.WriteLine("HP : " + HP);
+			print("HP : " + sr.ReadInt32());
+			print("name : " + sr.ReadString());
 
-			sw.Close();
+			fs.Close();
+			sr.Close();
 		}
 	}
 }
