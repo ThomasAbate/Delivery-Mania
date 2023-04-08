4ece8c80875ef3554515515c3e275618c0d95f3d
L M <80474693+Loris-Moreau@users.noreply.github.com>
2023-04-08 13:08:16 +0200

    [add] AI Patrol path & Bots

---
+++ Assets/Scripts/AI/PatrolPath.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPath : MonoBehaviour
+{
+	const float gizmoRadius = 0.4f;
+
+	private void OnDrawGizmos()
+	{
+		for(int i = 0; i < transform.childCount; i++) //goes from o to Child Count - 1 (0, 1, 2, etc...)
+		{
+			int j = GetNextIndex(i);
+
+			Gizmos.DrawSphere(GetWayPoint(i), gizmoRadius);
+			Gizmos.DrawLine(GetWayPoint(i), GetWayPoint(j));
+
+
+		}
+	}
+
+	private int GetNextIndex(int i)
+	{
+		if(i+1 == transform.childCount)
+		{
+			return 0;
+		}
+
+		return i + 1;
+	}
+
+	private Vector3 GetWayPoint(int i)
+	{
+		return transform.GetChild(i).position;
+	}
+}
