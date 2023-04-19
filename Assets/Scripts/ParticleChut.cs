9107ad49843c162041bb7d4d1d8ff9c084752f68
ThAb <117899380+ThomasAbate@users.noreply.github.com>
2023-04-19 17:26:12 +0200

    add feedbakc for box

--- Assets/Scripts/ParticleChut.cs
+++ Assets/Scripts/ParticleChut.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class ParticleChut : MonoBehaviour
 {
     public GameObject VFX;
+    private AudioSource SFX;
+    public AudioClip Boxfall;
+
+    private void Awake()
+    {
+        SFX = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             VFX.SetActive(true);
+            SFX.enabled= true;
+            AudioClip clip = GetRandomClip();
+            SFX.PlayOneShot(clip);
         }
     }
 
@@ -19,6 +29,14 @@ public class ParticleChut : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             VFX.SetActive(false);
+            SFX.enabled= false;
         }
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        SFX.volume = Random.Range(0.8f, 1f);
+        SFX.pitch = Random.Range(0.95f, 1.05f);
+        return Boxfall; 
     }
 }
