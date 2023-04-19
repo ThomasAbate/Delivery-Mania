bd306cf35c1be8f2b985dc2d9e7ebd2bb3c1a8db
L3o <74620207+leorchr@users.noreply.github.com>
2023-04-19 12:41:16 +0200

    [feat] open door

--- Assets/Scripts/Interactions/Door.cs
+++ Assets/Scripts/Interactions/Door.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Door : Interactive
 {
+    private bool open;
+    [SerializeField] private bool openSide;
     public override void OnInteraction()
     {
-        Debug.Log("bonjour");
-        transform.Rotate(transform.GetChild(0).position, 90f);
+        if (!open)
+        {
+            if(openSide)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            open = true;
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            open = false;
+        }
     }
 }
